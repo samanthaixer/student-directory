0718475ae55f028c52fcdc192b74b13b17d34434
Samantha Ixer <samantha.ixer@johnlewis.co.uk>
2019-02-28 16:17:35 +0000

    Added an interactive menu

--- directory.rb
+++ directory.rb
@@ -28,7 +28,7 @@ def input_students
       cohort = gets.chomp
     end
 
-    students << {name: name, cohort: cohort.to_sym, hobby: :sport, hobby: :coding, country_of_birth: :England, height: "Tall"}
+    students << {name: name, cohort: cohort.to_sym, hobby: :coding, country_of_birth: :England, height: "Tall"}
     puts students.count == 1 ? "Now we have #{students.count} student" : "Now we have #{students.count} students"
     #get another name from the user
     name = gets.gsub(/\R+/, '')
@@ -60,12 +60,33 @@ end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
-#nothing happens until we call the methods
-students = input_students
-
-if !students.empty?
-  print_header
-  #print(students)
-  print_by_cohort(students)
-  print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+      # 1. Print the menu and ask the user what to do
+      puts "1. Input the students"
+      puts "2. Show the students"
+      puts "9. Exit"
+      # 2. Read the input and save it to a variable
+      selection = gets.chomp
+      #3. Do what the user has asked
+      case selection
+      when "1"
+        students = input_students
+      when "2"
+        if !students.empty?
+          print_header
+          print(students)
+          print_footer(students)
+        end
+      when "9"
+        exit
+      else
+        puts "I don't know what you meant, try again"
+      end
+  end
 end
+
+#nothing happens until we call the methods
+interactive_menu
+#  print_by_cohort(students)
