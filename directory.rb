8f2f5c501ff15726211eda62c2a186723d7251b8
Samantha Ixer <samantha.ixer@johnlewis.co.uk>
2019-02-28 16:30:46 +0000

    Refactored code to have a clean interactive menu method

--- directory.rb
+++ directory.rb
@@ -1,8 +1,7 @@
+@students = [] # an empty array accessible to all methods
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create an empty array
-  students = []
   #get the first name
   name = gets.gsub(/\R+/, '')
   #while the name is not empty, repeat this code
@@ -28,62 +27,64 @@ def input_students
       cohort = gets.chomp
     end
 
-    students << {name: name, cohort: cohort.to_sym, hobby: :coding, country_of_birth: :England, height: "Tall"}
-    puts students.count == 1 ? "Now we have #{students.count} student" : "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort.to_sym, hobby: :coding, country_of_birth: :England, height: "Tall"}
+    puts @students.count == 1 ? "Now we have #{@students.count} student" : "Now we have #{@students.count} students"
     #get another name from the user
     name = gets.gsub(/\R+/, '')
   end
-  #return the array of input_students
-  students
 end
 def print_header
   puts "The students of Villains Academy"
   puts "--------------"
 end
-def print(students)
+def print_students_list
   index = 0
-  while index < students.length
-    if students[index][:name][0].upcase == "L"
-      if students[index][:name].length < 12
-        puts "#{index+1}. #{students[index][:name].center(25)} (#{students[index][:cohort]} cohort)"
+  while index < @students.length
+    #if @students[index][:name][0].upcase == "L"
+      if @students[index][:name].length < 12
+        puts "#{index+1}. #{@students[index][:name].center(25)} (#{@students[index][:cohort]} cohort)"
       end
-    end
+    #end
     index+=1
   end
 end
-def print_by_cohort(students)
-  sorted_students = students.sort_by{ |student| student[:cohort] }
+def print_by_cohort
+  sorted_students = @students.sort_by{ |student| student[:cohort] }
   sorted_students.each_with_index do |student, index|
     puts "#{index+1}. #{student[:name].center(25)} (#{student[:cohort]} cohort)"
   end
 end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+def show_students
+  if !@students.empty?
+    print_header
+    print_students_list
+    print_footer
+  end
+end
+def process(selection)
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
 end
 def interactive_menu
-  students = []
   loop do
-      # 1. Print the menu and ask the user what to do
-      puts "1. Input the students"
-      puts "2. Show the students"
-      puts "9. Exit"
-      # 2. Read the input and save it to a variable
-      selection = gets.chomp
-      #3. Do what the user has asked
-      case selection
-      when "1"
-        students = input_students
-      when "2"
-        if !students.empty?
-          print_header
-          print(students)
-          print_footer(students)
-        end
-      when "9"
-        exit
-      else
-        puts "I don't know what you meant, try again"
-      end
+    print_menu
+    process(gets.chomp)
   end
 end
 
